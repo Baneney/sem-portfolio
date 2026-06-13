f5a367b5e833f7f64114c46474bd2d10f3339190
Baneney <luizsemwarain@gmail.com>
2026-06-13 15:38:05 +0800

    snappier bottom contents

--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -12,7 +12,7 @@ function RevealText({ children, delay, className = '' }: { children: React.React
         initial={{ x: '-110%', opacity: 0 }}
         animate={{ x: '0%', opacity: 1 }}
         transition={{
-          duration: 0.6,
+          duration: 0.35,
           ease: [0.25, 0.1, 0.25, 1],
           delay,
         }}
@@ -49,7 +49,7 @@ export default function Hero({ showSplash }: { showSplash: boolean }) {
         setReveal(true)
         animationsDone.current = true
         container.style.overflowY = ''
-      }, 1000)
+      }, 650)
       return () => clearTimeout(t)
     }
   }, [lineInView, progress, showSplash])
